76082d21371a77b41feb184fc0b9b0eb6422047e
Richard Hopkins <hopkira@googlemail.com>
2025-02-26 23:59:54 +0000

    Merging K9 and Pimoroni code

--- ROS2_RP2040.py
+++ ROS2_RP2040.py
@@ -13,13 +13,51 @@
 #
 # python3 pyboard.py --device /dev/tty.usbmodem387A384631342 -f cp panel.py :main.py
 #
+#  Update the package dependencies in `package.xml`:
+# ```xml
+# <depend>rclpy</depend>
+# <depend>geometry_msgs</depend>
 #
+# Add an entry point for your node in `setup.py`. Replace the existing 
+# executable declaration with this:
+# ```python
+# entry_points={
+#        'console_scripts': [
+#                'my_mechanum_wheels = 
+#my_mechanum_wheels.RP2040_mechanum_controller_node:main'
+#        ],
+#},
+#
+# Build the package and source your ROS environment:
+# ```bash
+# cd ~/ros2_ws
+# colcon build --packages-select my_mechanum_wheels
+# source install/setup.bash
+#
+# Run the node to subscribe to `cmd_vel` topic:
+#```bash
+#ros2 run my_mechanum_wheels my_mechanum_wheels
+#
+# REACT TO MESSAGES
+# a. Subscribe to /cmd_vel Twist messages
+# b. Turn these into serial messages
+#
+# RECEIVE ODOMETER FRAMES FROM SERIAL
+# a. Publish odometer frames
+
 
 import serial
+import rclpy
 import ast
+import time
+from geometry_msgs.msg import Twist
 
-class MotorController():
+
+class MotorController(rclpy.Node):
     def __init__(self) -> None:
+        super().__init__('RP2040_mechanum_controller_node')
+        self.subscription = self.create_subscription(Twist, 
+            '/cmd_vel', self.listener_callback, 10)
         self.ser = serial.Serial(
             port='/dev/backpanel', # replace with your device name
             baudrate = 115200,
@@ -28,6 +66,19 @@ class MotorController():
             bytesize=serial.EIGHTBITS,
             timeout=1
             )
+        self.time_ms = int(time.time() * 1000)
+
+    def listener_callback(self, msg: Twist):
+        vx = msg.linear.x
+        vy = msg.linear.y
+        wz = msg.angular.z
+        front_left_velocity = (vx - vy - wz) / 3.0
+        front_right_velocity = (vx + vy + wz) / 3.0
+        rear_left_velocity = (vx + vy - wz) / 3.0
+        rear_right_velocity = (vx - vy + wz) / 3.0
+        # send four velocities to wheels
+        # odm,0,0,0,0
+        # self.__write(str.encode(twist+"\n"))
 
     def __write(self,text:str) -> None:
         #print("Back panel:",text)
@@ -41,3 +92,25 @@ class MotorController():
 
     def off(self):
         self.__write("off")
+
+    def send_odm(self) -> list:
+        odm = []
+        self.__write("odm")
+        my_input = self.ser.readlines()
+        #print("I heard:" + str(my_input))
+        try:
+            input_str = my_input[0].decode().strip()
+        except IndexError:
+            return odm
+        # read odm message
+        return odm
+
+def main(args=None):
+    rclpy.init(args=args)
+    RP2040_mechanum_controller_node = MotorController()
+    rclpy.spin(RP2040_mechanum_controller_node)
+    RP2040_mechanum_controller_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- RP2040_Motor_Controller.py
+++ RP2040_Motor_Controller.py
@@ -8,27 +8,199 @@
 # Quad Motor controller
 # running MicroPython
 #
+# RECEIVE CMD_VEL TWIST FORMAT MESSAGES VIA SERIAL
+# 1. Convert the Twist messages into wheel velocities
+#   vx = msg.linear.x
+#   vy = msg.linear.y
+#   wz = msg.angular.z
+#   front_left_velocity = (vx - vy - wz) / 3.0
+#   front_right_velocity = (vx + vy + wz) / 3.0
+#   rear_left_velocity = (vx + vy - wz) / 3.0
+#   rear_right_velocity = (vx - vy + wz) / 3.0
+# 2. Issue velocity commands to motors
+#
+# SEND ODOMETER FEEDBACK
+# 1. Read time values and encoder values
+# 2. Calculate velocity per second
+#    Linear Velocity (Vx): Vx = (FR + BR + FL + BL) / 4
+#    Angular Velocity (Vθ): `Vθ = (-FR + BR + FL - BL) / (4 * L)
+#    L is the distance between the wheels along each axis
+# 3. Use the robot's linear and angular velocities to calculate its
+#    position over time using the following equations
+#       X = X_previous + Vx * cos(θ) * dt
+#       Y = Y_previous + Vx * sin(θ) * dt
+#       θ = θ_previous + Vθ * dt
+# 4. Publish odometry messages containing the robot's position
+#    and velocity in the odom frame format via serial line
+#
+# Serial USB reading by Ben Renninson
+# https://github.com/GSGBen/pico-serial
+#
+# Motor 2040 velocity control by Christopher Parrott
+# https://github.com/pimoroni/pimoroni-pico/blob/main/micropython/examples/motor2040/velocity_control.py
+#
+
 from sys import stdin
 from time import sleep
 
 import uselect
-from machine import Pin
+import time
+# from machine import Pin
 
-# Serial USB reading by Ben Renninson
-# https://github.com/GSGBen/pico-serial
+import gc
+from motor import Motor, motor2040
+from encoder import Encoder, MMME_CPR
+from pimoroni import Button, PID, REVERSED_DIR
+
+# Wheel friendly names
+FL = 2
+FR = 3
+RL = 1
+RR = 0
+
+GEAR_RATIO = 50                         # The gear ratio of the motors
+COUNTS_PER_REV = MMME_CPR * GEAR_RATIO  # The counts per revolution of each motor's output shaft
+
+SPEED_SCALE = 5.4                       # The scaling to apply to each motor's speed to match its real-world speed
+
+UPDATES = 100                           # How many times to update the motor per second
+UPDATE_RATE = 1 / UPDATES
+TIME_FOR_EACH_MOVE = 2                  # The time to travel between each value
+UPDATES_PER_MOVE = TIME_FOR_EACH_MOVE * UPDATES
+PRINT_DIVIDER = 4                       # How many of the updates should be printed (i.e. 2 would be every other update)
+
+DRIVING_SPEED = 1.0                     # The speed to drive the wheels at, from 0.0 to SPEED_SCALE
+
+# PID values
+VEL_KP = 30.0                           # Velocity proportional (P) gain
+VEL_KI = 0.0                            # Velocity integral (I) gain
+VEL_KD = 0.4                            # Velocity derivative (D) gain
+
+
+# Free up hardware resources ahead of creating a new Encoder
+gc.collect()
+
+# Create a list of motors with a given speed scale
+MOTOR_PINS = [motor2040.MOTOR_A, motor2040.MOTOR_B, motor2040.MOTOR_C, motor2040.MOTOR_D]
+motors = [Motor(pins, speed_scale=SPEED_SCALE) for pins in MOTOR_PINS]
+
+# Create a list of encoders, using PIO 0, with the given counts per revolution
+ENCODER_PINS = [motor2040.ENCODER_A, motor2040.ENCODER_B, motor2040.ENCODER_C, motor2040.ENCODER_D]
+ENCODER_NAMES = ["RR", "RL", "FL", "FR"]
+encoders = [Encoder(0, i, ENCODER_PINS[i], counts_per_rev=COUNTS_PER_REV, count_microsteps=True) for i in range(motor2040.NUM_MOTORS)]
+
+# Reverse the direction of the B and D motors and encoders
+motors[FL].direction(REVERSED_DIR)
+motors[RL].direction(REVERSED_DIR)
+encoders[FL].direction(REVERSED_DIR)
+encoders[RL].direction(REVERSED_DIR)
+
+# Create the user button
+user_sw = Button(motor2040.USER_SW)
+
+# Create PID objects for position control
+vel_pids = [PID(VEL_KP, VEL_KI, VEL_KD, UPDATE_RATE) for i in range(motor2040.NUM_MOTORS)]
+
+# Helper functions for driving in common directions
+def drive_forward(speed):
+    vel_pids[FL].setpoint = speed
+    vel_pids[FR].setpoint = speed
+    vel_pids[RL].setpoint = speed
+    vel_pids[RR].setpoint = speed
+
+
+def turn_right(speed):
+    vel_pids[FL].setpoint = speed
+    vel_pids[FR].setpoint = -speed
+    vel_pids[RL].setpoint = speed
+    vel_pids[RR].setpoint = -speed
+
+
+def strafe_right(speed):
+    vel_pids[FL].setpoint = speed
+    vel_pids[FR].setpoint = -speed
+    vel_pids[RL].setpoint = -speed
+    vel_pids[RR].setpoint = speed
+
+
+def stop():
+    vel_pids[FL].setpoint = 0
+    vel_pids[FR].setpoint = 0
+    vel_pids[RL].setpoint = 0
+    vel_pids[RR].setpoint = 0
+
+# Enable the motor to get started
+for m in motors:
+    m.enable()
+
+update = 0
+print_count = 0
+sequence = 0
+
+captures = [None] * motor2040.NUM_MOTORS
+
 buffered_input = []
 input_line_this_tick = ""
 TERMINATOR = "\n"
 latest_input_line = ""
 
-patterns = {
-    "left": [],
-    "right": [],
-    "forward": [],
-    "back": [],
-    "spin_l": [],
-    "spin_r" : []
-}
+# Continually move the motor until the user button is pressed
+while not user_sw.raw():
+
+    # Capture the state of all the encoders
+    for i in range(motor2040.NUM_MOTORS):
+        captures[i] = encoders[i].capture()
+
+    for i in range(motor2040.NUM_MOTORS):
+        # Calculate the acceleration to apply to the motor to move it closer to the velocity setpoint
+        accel = vel_pids[i].calculate(captures[i].revolutions_per_second)
+
+        # Accelerate or decelerate the motor
+        motors[i].speed(motors[i].speed() + (accel * UPDATE_RATE))
+
+    # Print out the current motor values, but only on every multiple
+    if print_count == 0:
+        for i in range(motor2040.NUM_MOTORS):
+            print(ENCODER_NAMES[i], "=", captures[i].revolutions_per_second, end=", ")
+        print()
+
+    # Increment the print count, and wrap it
+    print_count = (print_count + 1) % PRINT_DIVIDER
+
+    update += 1     # Move along in time
+
+    # Have we reached the end of this movement?
+    if update >= UPDATES_PER_MOVE:
+        update = 0  # Reset the counter
+
+        # Move on to the next part of the sequence
+        sequence += 1
+
+        # Loop the sequence back around
+        if sequence >= 7:
+            sequence = 0
+
+    # Set the motor speeds, based on the sequence
+    if sequence == 0:
+        drive_forward(DRIVING_SPEED)
+    elif sequence == 1:
+        drive_forward(-DRIVING_SPEED)
+    elif sequence == 2:
+        turn_right(DRIVING_SPEED)
+    elif sequence == 3:
+        turn_right(-DRIVING_SPEED)
+    elif sequence == 4:
+        strafe_right(DRIVING_SPEED)
+    elif sequence == 5:
+        strafe_right(-DRIVING_SPEED)
+    elif sequence == 6:
+        stop()
+
+    time.sleep(UPDATE_RATE)
+
+# Stop all the motors
+for m in motors:
+    m.disable()
 
 def read_serial_input():
     global buffered_input, input_line_this_tick, TERMINATOR
@@ -47,6 +219,7 @@ def read_serial_input():
     else:
         input_line_this_tick = ""
 
+
 # main loop
 def main():
     '''
@@ -59,10 +232,20 @@ def main():
             latest_input_line = input_line_this_tick
             command = ""
             command = latest_input_line.strip()
-            # if the command is a light
-            # sequence then switch to that
-            # one and reset phase
-            if command in patterns:
-                pattern = command
+            if command == "odm":
+                dt = int(time.time() * 1000) - time_ms
+                time_ms = time_ms + dt
+                # get encoder difference
+                #
+                # Vx = (FR + BR + FL + BL) / 4
+                # Vθ = (-FR + BR + FL - BL) / (4 * L)
+                # X = X_previous + Vx * cos(θ) * dt
+                # Y = Y_previous + Vx * sin(θ) * dt
+                # θ = θ_previous + Vθ * dt
+                #
+                # write odm data back to the serial line
+
+            if command == "cmd_vel":
+                # write velocities to motors
 
 main()
